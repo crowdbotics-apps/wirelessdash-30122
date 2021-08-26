fef12475104d141d35358b500d4f224bbdd03f74
Crowdbotics <team@crowdbotics.com>
2021-08-26 12:20:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "wirelessdash_30122Identifier",
+  url: "https://wirelessdash_30122Identifier.botics.co",
+  api: "https://wirelessdash_30122Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
